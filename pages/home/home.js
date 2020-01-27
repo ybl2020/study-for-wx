cb7bbc761bbd027ba40332e6e11a643a6f999575
JXL <ybl20200126@gmail.com>
2020-01-27 17:18:16 +0800

    小程序初体验

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,31 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+    name: 'Coderwhy!',
+    age:'18',
+    students:[
+      {id:110,name:"kobe",age:30},
+      { id: 110, name: "kobe", age: 31 },
+      { id: 110, name: "kobe", age: 20 },
+      { id: 110, name: "why", age: 18}
+    ],
+    counter:0
   },
+  handleBtnClick(){
+    //1.错误做法：界面不会刷新！
+    //this.data.counter += 1,
+    //console.log("按钮发生了点击")
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+    //2.this.setData()
+    this.setData({
+      counter : this.data.counter + 1
+    }),
+    console.log("按钮发生了点击")
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+   handleSubtraction() {
+    this.setData({
+      counter: this.data.counter - 1
+    }),
+      console.log("按钮发生了点击")
   }
 })
